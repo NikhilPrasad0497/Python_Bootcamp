63869c5000e7d80127981f4b2290a817bd99459f
Nikhil Prasad <nikhilpd791@gmail.com>
2020-10-01 13:30:53 +0000

    Range, Enumerate

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ sum = 0
 for i in mylist:
   sum +=i
 print(sum)
+
+
+for i in range(10,0,-3):
+  print(i**2)
+
+for i,char in enumerate('Hello'):
+  print({i,char})
+
+
+for i,char in enumerate(list(range(100))):
+  if char ==50:
+    print(f'index of 50 is: {i}')
+  else:
+    continue
